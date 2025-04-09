528be78942bab2b350ed32ea9d15e3219195254b
codestutis <kestutisbiskis@gmail.com>
2025-04-09 12:11:41 -0400

    bruh

--- src/Bitboard.hpp
+++ src/Bitboard.hpp
@@ -8,22 +8,34 @@
 
 using namespace std;
 
-class Bitboard {
-  private:
-    enum PieceType {
-      whitePawns, whiteKnights, whiteBishops, whiteRooks, whiteQueens, whiteKing,
-      blackPawns, blackKnights, blackBishops, blackRooks, blackQueens, blackKing,
-    };
+class Bitboard
+{
+private:
+  enum PieceType
+  {
+    whitePawns,
+    whiteKnights,
+    whiteBishops,
+    whiteRooks,
+    whiteQueens,
+    whiteKing,
+    blackPawns,
+    blackKnights,
+    blackBishops,
+    blackRooks,
+    blackQueens,
+    blackKing,
+  };
 
-    uint64_t board[12];
+  uint64_t board[12];
 
-  public:
-    Bitboard();
-    void initialize(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-    string toString();
-    string getFEN();
-    void setSquare(uint64_t &board, int square);
-    char getSquare(int square);
+public:
+  Bitboard();
+  void initialize(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+  string toString();
+  string getFEN();
+  void setSquare(uint64_t &board, int square);
+  char getSquare(int square);
 };
 
 #endif // BITBOARD_HPP
